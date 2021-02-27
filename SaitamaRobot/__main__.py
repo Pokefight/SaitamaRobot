a288704774abe8ca424cb388ac3b81b9b20272d5
Pokefight <77969121+Pokefight@users.noreply.github.com>
2021-02-27 21:19:01 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -222,7 +222,7 @@ def start(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                text="☑️ Add Saitama to your group",
+                                text="☑️ Add me to your group",
                                 url="t.me/{}?startgroup=true".format(
                                     context.bot.username
                                 ),
